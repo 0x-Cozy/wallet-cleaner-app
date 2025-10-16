c0ceb20b24aedf4d5643009d4f8b647291201fe1
0x-Cozy <xcozydegen@gmail.com>
2025-10-16 12:49:25 +0100

    truncate

--- frontend/src/helpers/utils.ts
+++ frontend/src/helpers/utils.ts
@@ -3,10 +3,10 @@ const truncateString = (str: string | undefined | null): string | null => {
     if (!str || str === 'undefined') {
       return null;
     }
-    if (str.length <= 30) {
+    if (str.length <= 25) {
       return str;
     }
-    return str.slice(0, 18) + '...' + str.slice(-15);
+    return str.slice(0, 10) + '...' + str.slice(-12);
 };
 
 export default truncateString;
